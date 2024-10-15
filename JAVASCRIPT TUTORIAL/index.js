9d948b47ad08af01e8aaca4da71d815ef1c3614b
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-15 20:07:05 +0530

    js tutorial and js basics practice

---
+++ JAVASCRIPT TUTORIAL/index.js
@@ -0,0 +1,20 @@
+// variable basics 
+
+var number = 100;  //assigning a value to the variable 
+
+console.log(number);
+
+let a;  //declaring/defining a variable 
+
+a = 19;   //assigning a value to the variable 
+
+console.log(a);  // printing the  variable value
+
+
+
+let e = 1;  //we are declaring a variable and assigning a value 1 to it 
+
+e = 9;  //we are re-assigning var e' with a new value 
+
+console.log(e);
+
