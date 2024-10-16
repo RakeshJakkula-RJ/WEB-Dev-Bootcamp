f450a96e9333aa07660b993a2b7229993cabc8a5
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-16 18:00:50 +0530

    js basics tutorial

---
+++ JAVASCRIPT TUTORIAL/problem6.js
@@ -0,0 +1,14 @@
+// concatination (+)
+
+// if we use "+" operator in between numbers it is addition operator,
+// but if we use "+" in between to strings (to combine two strings) then it is "Concatination"
+
+//ex;
+
+let a = "Hello";
+let b = "World";
+let x = a+b;
+
+console.log(x);
+console.log(a+ " " +b);
+
