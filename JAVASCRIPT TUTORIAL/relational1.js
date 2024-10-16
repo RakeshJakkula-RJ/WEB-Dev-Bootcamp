983cbcdb52b5e8bb367312c9e343c890ec9822f6
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-16 18:02:11 +0530

    js basics tutorial

---
+++ JAVASCRIPT TUTORIAL/relational1.js
@@ -0,0 +1,29 @@
+// Relational Opereator 
+// a. > --->greater than 
+// b. < -----> less than 
+// c. >= -----> greater than or equal to 
+//d. <= ------> less than or equal to 
+
+let x = 10;
+let y = 5;
+
+// console.log(x>y);
+// console.log(x<y);
+
+let age1 = 24;
+let age2 = 24;
+
+// console.log(age1 >= age2);
+
+
+// let us assume you went to shopping to buy shoes of price 2999
+
+let shoePrice = 2999;
+let my_money = 2999;
+
+console.log(shoePrice>=my_money);
+console.log(shoePrice>my_money);
+console.log(shoePrice<my_money);
+console.log(shoePrice<=my_money);
+
+
