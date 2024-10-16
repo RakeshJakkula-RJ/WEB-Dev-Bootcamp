2479cb9193bf990feaa3a1dbd8c134d0d6177a70
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-16 17:58:37 +0530

    js basics tutorial

---
+++ JAVASCRIPT TUTORIAL/problem3.js
@@ -0,0 +1,28 @@
+// Naming Conventions 
+
+//1. Camel Case-----> to define variable names 
+// ex.
+
+let userName = "Robby";
+let myFirstName = "Keene";
+
+console.log(userName);
+
+// 2. Snake Case----> variable 
+//ex :
+
+let user_name = "Migiuel";
+let my_first_name = "Diaz";
+
+console.log(user_name);
+
+let MY_MOTHER_NAME = "carmen";  // we use all capital cases for constant values 
+
+//3. Pascal case-------->variable 
+// ex,
+
+let UserName = "Hawk";
+let MyFirstName = "Eli";
+
+console.log(UserName);
+
