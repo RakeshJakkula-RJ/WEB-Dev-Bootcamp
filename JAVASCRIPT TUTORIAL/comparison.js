dcab54e7550f484d9c807c25807820d07941f116
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-16 18:02:56 +0530

    js basics tutorial

---
+++ JAVASCRIPT TUTORIAL/comparison.js
@@ -0,0 +1,62 @@
+// Comparison Operator (input can be number,string) (output will always be boolean)
+// a.double equal (==)
+// b.not equal (!=)
+// c.triple equal(===)
+// d.not double equal(!==)
+// single equal to (=) is to assign a value to variable 
+// double equal to (==) is to compare two values 
+
+// a.double equal(==)   
+
+let a = 10;
+
+let b = 2;
+
+let c = "goku"
+
+let d = "10";
+
+let e = "Goku";
+
+console.log(a==b);
+
+console.log(a==d);
+
+console.log(c==e);
+
+// b.not equal(!=)
+
+let x = "goku";
+let y = "Goku";
+
+console.log(x!=y);
+
+let u = 9;
+let v = "9";
+
+console.log(u!=v);
+
+// c.triple equal(===) 
+// when we use triple equal between two values it should match value and also its data type
+
+let r = 2;
+let s = "2";
+console.log(r===s);
+
+console.log("2"==="3");
+
+console.log("hawk"==="hawk");
+
+//not double equal(!==)
+// any one of data type or value need to be match or same 
+
+let a1 = "2";
+let b1 = 2;
+
+console.log(a1!==b1);
+
+console.log("2"!=="2");
+
+
+
+
