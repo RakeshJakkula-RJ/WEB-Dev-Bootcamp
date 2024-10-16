c5a46d3f72fbdbbb51a58983aa76b6ffef668d0e
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-16 18:01:33 +0530

    js basics tutorial

---
+++ JAVASCRIPT TUTORIAL/problem7.js
@@ -0,0 +1,17 @@
+// Data Types 
+//1. Number = 0,1,123
+//2. String = " " or ' '
+//3. Boolean = True or False 
+
+let x = true;
+ console.log(x);
+
+ console.log(typeof(x));
+
+ // new line operator (\n)
+ //the new line operator symbol should also  be inside string "\n"
+
+ let a = "hello";
+ let b = "world";
+
+ console.log(a + "\n" + b);
