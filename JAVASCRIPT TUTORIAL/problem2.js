d8066bc4b5b1c74d4993ffbbe06a7b6a0ce2f2de
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-16 17:57:55 +0530

    Js basics tutorial

---
+++ JAVASCRIPT TUTORIAL/problem2.js
@@ -0,0 +1,44 @@
+//Data Types 
+// there are different data types 
+
+// 1.Number 
+// 2.String------>characters
+
+let name = "Rakesh";
+
+console.log(name);
+
+let dob = "25-06-2000";
+console.log(dob);
+
+// to check the data types stored in a variable 
+
+//typeof()
+
+console.log(typeof(name));
+console.log(typeof(dob));
+
+let a = 10;
+let b = 11;
+console.log(a,b);
+
+// rules to follow when creating variable names 
+
+// 1.A variable cannot start with a digit
+//a. ex : let 123
+//2. A variable cannot start with any symbol 
+//a. ex : @#&%
+// b. exeption for  $ and _
+//3. A variable names should be self-explanatory 
+//4. A variable name can only contain alpha-numeric characters and underscores (a-z, A-Z, 0-9, _ $)
+
+//5. Variable names are case sensitive 
+// a. ex abc is not equal to ABC
+
+// 6. Variable names cannot be keywords 
+// ex : let , const , console, log, var, type of 
+
+// 7.Variable with multiple words cannot be seperated by space 
+//ex : let user name this is wrong 
+
+
