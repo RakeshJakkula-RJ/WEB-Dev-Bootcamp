32c4037d39a4644387a3ddc029f326061ebc5414
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-16 17:59:14 +0530

    js basics tutorial

---
+++ JAVASCRIPT TUTORIAL/problem4.js
@@ -0,0 +1,22 @@
+// Mathematical Operators 
+// addition , subtraction , multiplication . division , percentage/modulo
+// + , - , * , / , % 
+
+let a = 8;
+let b = 2;
+
+console.log("the sum is", a+b);
+
+console.log("the difference is", a-b);
+
+console.log("the multiplaction is", a*b);
+
+console.log("the division is", a/b);
+
+//modulo operator----> we are looking for remainder, we do not calculate decimals.
+
+let d = 10;
+let e = 20;
+
+console.log("the modulous of d,e is", d%e);
+
