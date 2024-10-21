ad3a4f9c21964b0d97425e4ce993b5990827162e
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-21 15:55:12 +0530

    js basics

---
+++ JAVASCRIPT TUTORIAL/pblm1.js
@@ -0,0 +1,36 @@
+// if-else statement : if the given problem has two conditions,
+//to check then we will use  if else statement.
+
+// syntax
+
+//if(condition){
+   // if condition is true 
+   // this will print 
+     // }
+   // else{
+        // if thr condition is false this will print
+          //}
+
+     //ex:
+
+     let homework = false;
+
+     if(homework){
+         console.log("i am allowed to play outside");
+     } 
+     else{
+        console.log("i am not allowed to play outside");
+     }
+
+     // write a condition which is greater 
+
+     let a = 9;
+     let b = 19;
+
+     if(a>b){
+        console.log("a is greater");
+     }
+     else{
+        console.log("b is greater");
+     }
+    
