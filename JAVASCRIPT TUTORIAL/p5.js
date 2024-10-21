50189f6f745515d100cb3379b8616c1ab096e07a
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-21 15:58:22 +0530

    js basics

---
+++ JAVASCRIPT TUTORIAL/p5.js
@@ -0,0 +1,16 @@
+// Problem1
+
+// if a number is divisible by 3, then "print multiple of 3",
+// "else not a multiple of 3"
+
+
+// let number = 9; 3%3==0;
+
+let number = 5;
+
+if(number % 3==0){
+    console.log("multiple of 3");
+}
+else{
+    console.log("not a multiple of 3");
+}
