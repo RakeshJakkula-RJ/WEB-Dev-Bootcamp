c92e589869da619d1c20d495d80a82dc8d4dd911
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-21 15:53:18 +0530

    js basics tutorial

---
+++ JAVASCRIPT TUTORIAL/while.js
@@ -0,0 +1,39 @@
+// While loop
+
+ let number = 1;
+
+ while(number <= 5){
+    console.log("hello");
+     number++;
+ }
+
+ //print from 10 to 1;
+
+ let a = 10;
+
+ while(a>=1){
+    console.log(a);
+     a--; 
+ }
+
+ // print even numbers from 0 to 10; using while loop 
+
+ let x = 0;
+
+ while(x <= 10){
+    if(x %2 == 0){
+        console.log(x);
+    }
+    x++;
+ }
+
+ // print the numbers which are divisible by 3 in between 0-10;
+
+ let y = 0;
+
+ while(y <= 10){
+    if(y%3==0){
+        console.log(y);
+    }
+    y++;
+ }
