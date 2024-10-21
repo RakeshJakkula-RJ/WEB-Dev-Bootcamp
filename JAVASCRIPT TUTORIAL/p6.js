b5c509adf6e5af3a5e8643d380b62a2fa34bb261
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-21 15:58:59 +0530

    js basics

---
+++ JAVASCRIPT TUTORIAL/p6.js
@@ -0,0 +1,15 @@
+// check if a person is allowed o drive in india,
+//if yes,then print "yes eligible to drive" -----> age >= 18 eligible
+// else "not eligible to drive " -----> age < 18 not eligible
+
+let age_of_person = 24;
+
+// let age_of_person = 17;
+
+
+if(age_of_person >= 18){
+    console.log("eligible to drive");
+}
+else{
+    console.log("not eligible to drive ");
+}
