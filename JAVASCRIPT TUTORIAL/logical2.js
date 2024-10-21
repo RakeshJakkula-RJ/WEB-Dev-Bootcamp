2904c28e17c39a0fe81c3201a58536d31ff19cc0
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-21 15:51:38 +0530

    js basics tutorial

---
+++ JAVASCRIPT TUTORIAL/logical2.js
@@ -0,0 +1,19 @@
+
+let a = 9;
+
+let b = 199;
+
+console.log(a==b || b>a);
+
+
+let x = "11111";
+
+let y = 11111;
+
+console.log( a>b || a!=b);
+
+let a1 = "Robby";
+
+let b1 = "Keene";
+
+console.log(a1<b1 || a1==b1);
