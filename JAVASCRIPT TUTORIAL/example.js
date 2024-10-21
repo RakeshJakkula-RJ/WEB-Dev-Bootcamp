03f517b9160504b0dd0a9eec6753dc41846d7131
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-21 15:49:17 +0530

    js basics tutorial

---
+++ JAVASCRIPT TUTORIAL/example.js
@@ -0,0 +1,14 @@
+  
+  let number = 9; // check weather given number is even or odd ?
+
+  if(number%2==0){
+    console.log("given number is even");
+  }
+  else{
+    console.log("given number is odd")
+  }
+
+  // ternary operater
+  let number1 = 8;
+
+  (number1%2==0) ? console.log("given number is even") : console.log("given number is odd")
