96bedb0d6d914297acfdc92f8aa511f892b3105d
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-21 15:55:51 +0530

    js basics

---
+++ JAVASCRIPT TUTORIAL/pblm2.js
@@ -0,0 +1,17 @@
+//let me know if you can buy the item or not 
+
+// if you can buy the item print "i bought the item"
+
+// else print "i dont have enough money to buy an item"
+
+let item_price = 4999;
+
+let myMoney = 45000;
+
+if(myMoney>=item_price){
+    console.log("i bought the item");
+}
+else{
+    console.log("i dont have enough money to buy an item");
+}
+
