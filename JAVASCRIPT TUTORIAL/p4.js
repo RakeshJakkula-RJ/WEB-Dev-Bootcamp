7cbdaa019588720f386ffb4987b4e14f426abb3c
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-21 15:57:44 +0530

    js basics

---
+++ JAVASCRIPT TUTORIAL/p4.js
@@ -0,0 +1,22 @@
+
+// let traffic_light = "orange";
+
+// let traffic_light = "yellow";
+
+// let traffic_light = "red";
+
+let traffic_light = "green";
+
+
+if(traffic_light === "red"){
+    console.log("please stop...");
+}
+else if(traffic_light === "yellow"){
+    console.log("be ready to go...");
+}
+else if(traffic_light === "green"){
+    console.log("please go...")
+}
+else{
+    console.log("erorr...")
+}
