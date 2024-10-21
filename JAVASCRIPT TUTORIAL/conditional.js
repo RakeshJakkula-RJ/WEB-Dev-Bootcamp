6825fd6b7b98eef9f0e63b51b882d033747cc7d7
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-21 15:48:27 +0530

    js basics tutorial

---
+++ JAVASCRIPT TUTORIAL/conditional.js
@@ -0,0 +1,22 @@
+// Syntax 
+
+// if(condition){
+   // if the condition is true
+   // the compiler will come inside 
+   // the if statement. 
+   // }
+
+   // if the given pblm has one condition then we will use if statement.
+
+
+   if(2 <= 3){
+      console.log("2 is less than 3");
+   }
+
+
+   let a = 5;
+   let b = 4;
+
+   if(a!=b){
+       console.log("hello everyone...!");
+   }
