6adbd5e9a689de5b98b09b2f5e74b10fcc9b64ea
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-21 15:56:42 +0530

    js basics

---
+++ JAVASCRIPT TUTORIAL/p3.js
@@ -0,0 +1,22 @@
+// if else if else statement : if the given problem has more than two conditions,
+// to check then we will use if else if else statement.
+
+// let supermarket = "sensT";
+let supermarket = "senstivity";
+
+
+if(supermarket === "colgate"){
+    console.log("pleasde give  colgate toothpaste");
+}
+else if(supermarket === "sensT"){
+    console.log("please give  sesT toothpaste");
+}
+else if(supermarket === "closeup"){
+    console.log("please give closeup toothpaste");
+}
+else if(supermarket === "dabur"){
+     console.log("please give dabur toothpaste");
+}
+else{
+    console.log("return back to home");
+}
