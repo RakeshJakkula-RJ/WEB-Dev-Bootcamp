60adeedc91013dd3433460dbfb7e9139160b7bae
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-21 15:54:38 +0530

    js basics

---
+++ JAVASCRIPT TUTORIAL/switch.js
@@ -0,0 +1,21 @@
+// Switch Case 
+
+
+// let customerTypesNumber = 9;
+// let customerTypesNumber = 1;
+let customerTypesNumber = 3;
+
+switch(customerTypesNumber){
+    case 1:
+        console.log("you have choosen English");
+          break;
+    case 2:
+        console.log("you have choosen hindi");
+        break;
+    case 3:
+        console.log("you have choosen telugu");
+        break;
+    default :
+        console.log("wrong input...");
+     
+}
