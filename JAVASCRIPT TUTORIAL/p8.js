732cb61c2718601c1b92870bc4b75cbd8eccba67
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-21 16:01:23 +0530

    js basics

---
+++ JAVASCRIPT TUTORIAL/p8.js
@@ -0,0 +1,19 @@
+// Ternary Operator 
+
+// syntax : (condition) ? true statement : false statement;
+
+// example 
+
+let a = "139";
+
+let b = 139;
+
+(a==b) ? console.log("a and b are equal") : console.log("a and b are not equal");
+
+let x = 9999;
+
+let y = 999;
+
+(x>=y) ? console.log("x is greather than y") : console.log("y is greater than x");
+
+
