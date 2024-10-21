c9b128c220a42fec32d95429a1d4999a0fea97bc
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-21 16:02:01 +0530

    js basics

---
+++ JAVASCRIPT TUTORIAL/p9.js
@@ -0,0 +1,18 @@
+// Logical Operators : 
+// 1.And Operator(&&)
+//2.OR Operator (||)
+// 3. Not Operator (!)
+
+
+let storedUserName = "Miguel Diaz";
+let storedPassword = "139";
+
+let inputUsername = "Miguel Diaz"
+let inputPassword = "139";
+
+if(storedUserName === inputUsername && storedPassword === inputPassword){
+         console.log("login successfull...");
+}
+else{
+    console.log("please check your userName or password");
+}
