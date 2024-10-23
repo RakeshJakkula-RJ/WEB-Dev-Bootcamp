ac3c6a02219d7ff54aefe8f9f7e85c365d3804c5
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-23 19:07:43 +0530

    Js Basics

---
+++ JAVASCRIPT TUTORIAL/object2.js
@@ -0,0 +1,20 @@
+
+let form = {
+    name    : "Rakesh RJ",
+father_name  : "srinivas",
+    age     : 24,
+ had_dinner : false,  
+    hobbies : ["Cricket", "Movies", "Bgmi"],
+    address : {
+        city : "GDK",
+        state : "TG",
+        district : "Peddapelli",
+        pincode : 505209, 
+    }
+};
+
+// console.log(form);
+
+console.log(form.hobbies[0]);
+
+console.log(form.address.city);
