73962f2c512d1bffcf572dff30c8622ddc5bdc32
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-23 19:04:06 +0530

    Js Basics

---
+++ JAVASCRIPT TUTORIAL/nested2.js
@@ -0,0 +1,16 @@
+
+// 3 farms print the 5 seeds in horizontal line * * * * *;
+
+
+
+for(let farm=1; farm<=3; farm++){
+     
+    console.log("farm", farm);
+
+    let bag = "";
+
+    for(let seed=1; seed<=5; seed++){
+        bag = bag + "*" + " ";
+    }
+    console.log(bag);
+}
