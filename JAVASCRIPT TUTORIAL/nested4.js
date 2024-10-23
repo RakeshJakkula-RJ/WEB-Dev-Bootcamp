299eff1f054b921c9c5f8a8bcece390bf2d5eea9
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-23 19:05:27 +0530

    Js Basics

---
+++ JAVASCRIPT TUTORIAL/nested4.js
@@ -0,0 +1,30 @@
+
+// **********
+// *        *
+// *        *
+// *        *
+// *        *
+// *        *
+// *        *
+// *        *
+// *        *
+// **********
+
+
+for(hor=1; hor<=10; hor++){
+
+    let bag = "";
+
+    for(ver=1; ver<=10; ver++){
+        if(hor==1 || hor==10){
+            bag = bag + "*";
+        }
+        else if(ver==1 || ver==10){
+            bag = bag + "*";
+        }
+        else{
+            bag = bag + " ";
+        }
+    }
+     console.log(bag);
+}
