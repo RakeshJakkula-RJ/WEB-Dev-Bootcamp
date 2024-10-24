792de7c8f31bab88890a6fce59909c80958a21a5
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-24 20:53:01 +0530

    Js Basics

---
+++ JAVASCRIPT TUTORIAL/function3.js
@@ -0,0 +1,17 @@
+
+// write a funcvtion to check if a umber is even or odd.
+
+function checkEvenOrOdd(number){
+
+    if(number%2==0){
+
+        console.log(number, "is even");
+    }
+    else{
+        console.log(number, "is odd");
+    }
+}
+
+ for(let i=1; i<=10; i++){
+    checkEvenOrOdd(i);
+ }
