d2c5fafa64ad606ecca74eec0178e5a20723a603
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-24 20:47:14 +0530

    Js Basics

---
+++ JAVASCRIPT TUTORIAL/function.js
@@ -0,0 +1,34 @@
+// Function :
+// A function is a piece of code or block of code which can be re-used multiple times.
+
+// {
+
+//     Block of code
+// }
+
+function Instagram(){
+
+    console.log("please open instagram");
+  // this will return or print nothing because it is just a function declaration 
+} // js will not execute the function declaration 
+
+   Instagram(); // now,this is where js will call instagram function 
+        // function calling or calling a function.
+
+        // Instagram();
+        // Instagram();
+        // Instagram();
+        // Instagram();
+        // Instagram();   // the code can be written once and re-used multiple times.
+
+
+
+    function whatsApp(){
+
+        console.log("please open whatsApp");
+    }
+
+    whatsApp();
+    whatsApp();
+    whatsApp();
+    whatsApp();
