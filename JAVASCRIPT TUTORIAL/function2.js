435ca4230cdec9735d6a86ed3ec2fc1592dc7ee9
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-24 20:52:15 +0530

    Js Basics

---
+++ JAVASCRIPT TUTORIAL/function2.js
@@ -0,0 +1,21 @@
+// function logic should be written in a way that the code can be re-used many times.
+
+// to access the app name we have to initialize a variable 
+// inside the function's constructor as a parameter.
+
+function openApp(name){
+
+    console.log("please open the", name);
+}
+
+openApp("Instagram");
+
+openApp("whatsApp");
+
+function Call(name){
+
+    console.log("please call", name);
+
+}
+
+   Call("RJ")
