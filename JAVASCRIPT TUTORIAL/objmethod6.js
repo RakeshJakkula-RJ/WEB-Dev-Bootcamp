886c53384d4c6577155474b1f75b44d9f3be18a8
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-10-30 15:47:27 +0530

    Js Basics

---
+++ JAVASCRIPT TUTORIAL/objmethod6.js
@@ -0,0 +1,14 @@
+
+// Rest operator : Rest operator is used for a function,
+//  when we want to convert the arguments into a array(...b)
+
+// Spread operator : spread operatror is used to create a copy of array, object(...)
+
+// syntax for both spread operator and rest operator are same (...) but used in two different scenarios.
+
+function print(...b){
+
+    console.log(b);
+};
+
+print(1,2,3,4,5,6,7,8,9,10,11,12);
