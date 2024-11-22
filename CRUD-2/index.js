21a6f5d6dac931709200d8ba3f0a4f55f73b55a1
Rakesh RJ <rakeshjakkula139@gmail.com>
2024-11-22 19:32:43 +0530

    advance js basics

---
+++ CRUD-2/index.js
@@ -0,0 +1,14 @@
+
+import navbar from "./Components/navbar.js";
+
+
+let post_div = document.getElementById("post");
+
+let nav_div = document.getElementById("navbar");
+
+  nav_div.innerHTML = navbar();
+
+
+
+
+
